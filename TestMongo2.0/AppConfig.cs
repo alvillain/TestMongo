6c40a48d53738289d416c7411ef05ad575a53fd3
Alexander Skarupskiy <skarupskiy@skycom.com>
2018-08-21 12:26:20 +0200

    added simple c# mongo client with endless reading operation

---
+++ TestMongo2.0/AppConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace TestMongo2._0
+{
+    public class AppConfig
+    {
+        public string MongoConnectionString { get; set; }
+        public string MongoDb { get; set; }
+        public string CollectionName { get; set; }
+        public string FilteredObjectId { get; set; }
+        public int WaitIntervalSeconds { get; set; }
+    }
+}
---
+++ TestMongo2.0/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace TestMongo2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            var appConfig = builder.Build().GetSection("AppConfig").Get<AppConfig>();
+
+            var tokenSource = new CancellationTokenSource();
+            var client = new MongoClient(appConfig.MongoConnectionString);
+            var database = client.GetDatabase(appConfig.MongoDb);
+            var collection = database.GetCollection<BsonDocument>(appConfig.CollectionName);
+            
+            while (true)
+            {
+                var filter = Builders<BsonDocument>.Filter.Eq("_id", new ObjectId(appConfig.FilteredObjectId));
+
+                var res =  collection.Find(filter);
+               
+                Console.WriteLine($"Read {res.ToList().Count} and wait {appConfig.WaitIntervalSeconds} seconds...");
+
+                WaitHandle.WaitAny(new[] { tokenSource.Token.WaitHandle }, TimeSpan.FromSeconds(appConfig.WaitIntervalSeconds));
+            }
+        }
+    }
+}
